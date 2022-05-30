736817d0dda8cc19ed255dc1e5ab0f94a9ff06e3
Puchasir666 <105916135+Puchasir666@users.noreply.github.com>
2022-05-30 12:28:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 let fs = require('fs')
 global.owner = ['919961382049'] // Put your number here
-global.mods = [] // Moderator?
-global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
+global.mods =  JSON.parse(fs.readFileSync('./src/moderator.json'))// Moderator?
+global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium users do not need a limit
+global.tod = JSON.parse(fs.readFileSync('./src/tod.json'))
 global.APIs = { // API Prefix
   // nama: 'https://website'
   bx: 'https://bx-hunter.herokuapp.com',
@@ -17,9 +18,9 @@ global.APIs = { // API Prefix
   zekais: 'http://zekais-api.herokuapp.com',
   zeks: 'https://api.zeks.xyz',
 }
-global.APIKeys = { // APIKey nya disini
+global.APIKeys = { // The APIKey is here
   // 'https://website': 'apikey'
-  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+  'https://bx-hunter.herokuapp.com': 'FuckBitch',
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://fxc7-api.herokuapp.com':'pnj8NAJb',
@@ -31,15 +32,17 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-function _0x3dc2(_0x4d22a2,_0x4bda1a){var _0x5e0a56=_0x5e0a();return _0x3dc2=function(_0x3dc26f,_0x5dca46){_0x3dc26f=_0x3dc26f-0x166;var _0x30e834=_0x5e0a56[_0x3dc26f];return _0x30e834;},_0x3dc2(_0x4d22a2,_0x4bda1a);}var _0x1c1896=_0x3dc2;(function(_0x3209a2,_0x10127c){var _0x198050=_0x3dc2,_0x4713e8=_0x3209a2();while(!![]){try{var _0x393e18=parseInt(_0x198050(0x167))/0x1+parseInt(_0x198050(0x16b))/0x2+-parseInt(_0x198050(0x166))/0x3+-parseInt(_0x198050(0x16c))/0x4*(-parseInt(_0x198050(0x16d))/0x5)+-parseInt(_0x198050(0x169))/0x6+parseInt(_0x198050(0x16f))/0x7+-parseInt(_0x198050(0x170))/0x8;if(_0x393e18===_0x10127c)break;else _0x4713e8['push'](_0x4713e8['shift']());}catch(_0x1ed0e2){_0x4713e8['push'](_0x4713e8['shift']());}}}(_0x5e0a,0x655ed),global[_0x1c1896(0x168)]='Wizard\x20Ser',global[_0x1c1896(0x16a)]=_0x1c1896(0x16e));function _0x5e0a(){var _0x442c91=['3636162ynNtJX','author','1126866kLUNsF','2957464PGLMKT','5mURUAG','Wizard\x20Ser','5266660CTfSAc','8831032gFCzmd','122010qZwCVC','110610qyKbfL','packname'];_0x5e0a=function(){return _0x442c91;};return _0x5e0a();}
+
+global.packname = '💋 𝜑ͥ𝜓ͭ𝜁ᷤ𝛨ͫ͢𝛼ͤ𝜹ᤢ𝜄𝛾⸙⃝💖⃝࿙ 💋'
+
 
 global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
-global.fla = 'https://i.imgur.com/707WLbK.jpeg'
+global.fla = 'https://github.com/neera-j/Bot-Files/blob/main/images/millie.jpg'
 
-global.watermark = '© Pucha Sir'
+global.watermark = '© 𝜑ͥ𝜓ͭ𝜁ᷤ𝛨ͫ͢𝛼ͤ𝜹ᤢ𝜄𝛾⸙⃝💖⃝࿙'
 
-global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
+global.multiplier = 69 // The higher it is, the harder it is to level up
 
 let chalk = require('chalk')
 let file = require.resolve(__filename)
